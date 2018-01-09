98e35aa12da5e7e7e8eacdeab26ff9e10fcc885f
None <None>
2018-01-09 12:06:39 -0800

    setup script works when called from anywhere

--- mn-setup/hostfiles/setup.sh
+++ mn-setup/hostfiles/setup.sh
@@ -1,34 +1,35 @@
 #!/bin/bash
 
-# script to make persistent dirs inside ../proj/hostfiles
+# script to make persistent dirs inside ../hostfiles/
+dir=`dirname "$BASH_SOURCE"`
 dirCount=0
 for((i=1;i<11;i++)); do
 	# check if directories exist; create them if not there
-	if [ -d "/home/mininet/mininet/proj/hostfiles/h${i}" ]; then
+	if [ -d "$dir/h${i}" ]; then
 		echo "h${i}/ directory already exists. Skipping."
 	else
-		mkdir /home/mininet/mininet/proj/hostfiles/h${i}
+		mkdir $dir/h${i}
 		let dirCount="dirCount += 1"
 	fi
 
-	if [ -d "/home/mininet/mininet/proj/hostfiles/h${i}/var" ]; then
+	if [ -d "$dir/h${i}/var" ]; then
                 echo "h${i}/var directory already exists. Skipping."
         else
-                mkdir /home/mininet/mininet/proj/hostfiles/h${i}/var
+                mkdir $dir/h${i}/var
 		let dirCount="dirCount += 1"
         fi
 
-	if [ -d "/home/mininet/mininet/proj/hostfiles/h${i}/var/run" ]; then
+	if [ -d "$dir/h${i}/var/run" ]; then
                 echo "h${i}/var/run directory already exists. Skipping."
         else
-                mkdir /home/mininet/mininet/proj/hostfiles/h${i}/var/run
+                mkdir $dir/h${i}/var/run
 		let dirCount="dirCount += 1"
         fi
 
-	if [ -d "/home/mininet/mininet/proj/hostfiles/h${i}/var/log" ]; then
+	if [ -d "$dir/h${i}/var/log" ]; then
                 echo "h${i}/var/log directory already exists. Skipping."
         else
-                mkdir /home/mininet/mininet/proj/hostfiles/h${i}/var/log
+                mkdir $dir/h${i}/var/log
 		let dirCount="dirCount += 1"
         fi
 
@@ -36,7 +37,7 @@ for((i=1;i<11;i++)); do
 	# echo `mkdir h${i} h${i}/var h${i}/var/run h${i}/var/log`
 
 	# cp timer.sh to host root dir. Do not check if already exists so that user can overwrite each host's files easily if he/she makes changes to timer.sh.
-	cp /home/mininet/mininet/proj/hostfiles/timer.sh /home/mininet/mininet/proj/hostfiles/h${i}/
+	cp $dir/timer.sh $dir/h${i}/
 	echo "timer.sh copied into h${i}/"
 done
 echo "$dirCount missing directories created."
